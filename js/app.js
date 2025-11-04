dc03fd630d67a6bf2e6beb5027cb0555e8168f9e
pycheol07-create <pycheol07@gmail.com>
2025-11-04 11:48:26 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -224,8 +224,10 @@ export let context = {
     tempSelectedMembers: [],
     memberToSetLeave: null,
     memberToCancelLeave: null,
-    activeMainHistoryTab: 'work',
+    aactiveMainHistoryTab: 'work',
     attendanceRecordToDelete: null,
+    isMobileTaskViewExpanded: false, // 👈 [추가] 모바일 업무카드 '전체보기' 상태
+    isMobileMemberViewExpanded: false, // 👈 [추가] 모바일 팀원현황 '전체보기' 상태
 };
 
 export let appState = {
@@ -400,7 +402,8 @@ export const updateElapsedTimes = () => {
 /** (모든 모듈에서 import) */
 export const render = () => {
   try {
-    renderRealtimeStatus(appState, appConfig.teamGroups, appConfig.keyTasks || []);
+    // 👈 [수정] context의 모바일 뷰 상태 값을 파라미터로 전달
+    renderRealtimeStatus(appState, appConfig.teamGroups, appConfig.keyTasks || [], context.isMobileTaskViewExpanded, context.isMobileMemberViewExpanded);
     renderCompletedWorkLog(appState);
     updateSummary(appState, appConfig); 
     renderTaskAnalysis(appState, appConfig); 
@@ -764,7 +767,8 @@ async function main() {
   
   // ✅ [추가] 1분(60000ms)마다 페이지 자동 새로고침 (기존 로직 유지)
   setInterval(() => {
-    const activeModal = document.querySelector('.fixed.inset-0.z-50:not(.hidden), .fixed.inset-0.z-\[60\]:not(.hidden), .fixed.inset-0.z-\[99\]:not(.hidden)');
+    // 👈 [수정] z-index를 모두 제거하고 공통 클래스(.fixed.inset-0)만 선택
+    const activeModal = document.querySelector('.fixed.inset-0:not(.hidden)');
     if (!activeModal) { 
         location.reload();
     } else {
